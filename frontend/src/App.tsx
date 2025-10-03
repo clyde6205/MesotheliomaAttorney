cdd9f45283e38312280b9ec38c3c02c0c8b209e7
clyde6205 <clydeusa@zohomail.com>
2025-10-03 16:13:54 +0800

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import LeadForm from './components/LeadForm';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Mesothelioma Attorney Intake</h1>
+      <LeadForm />
+    </div>
+  );
+}
